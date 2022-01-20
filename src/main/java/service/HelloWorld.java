501a68ad2d287591a130a1f9b684909e8d226610
Chloe <chloe@Chloede-MacBook-Pro.local>
2022-01-20 13:01:23 -0800

    add base model for maven

---
+++ src/main/java/service/HelloWorld.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+
+//  protected final Logger = LogManager.getLogger(HelloWorld.class);
+
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+//        Logger.debug("Debug log message");
+//        Logger.info("Info log message");
+//        Logger.error("Error log message");
+    }
+}
